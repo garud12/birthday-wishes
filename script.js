bda565366bf64f4f31dfc25861357d063d19bf01
garud12 <91959161+garud12@users.noreply.github.com>
2021-11-20 20:00:20 +0530

    Add initial birthday wishes website files

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "dinesh patil" && b=="22-11-1984")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
